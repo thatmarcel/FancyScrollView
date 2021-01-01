6a06048b7513f14ce28129fd0a85f2eff5c59f9f
Marcel Braun <contact@thatmarcel.com>
2021-01-01 15:02:11 +0100

    hmmmm

--- Sources/FancyScrollView/HeaderScrollView.swift
+++ Sources/FancyScrollView/HeaderScrollView.swift
@@ -10,6 +10,7 @@ struct HeaderScrollView: View {
     let headerHeight: CGFloat
     let scrollUpBehavior: ScrollUpHeaderBehavior
     let scrollDownBehavior: ScrollDownHeaderBehavior
+    let headerStateCallback: BoolCallback?
     let header: AnyView
     let content: AnyView
 
@@ -19,6 +20,7 @@ struct HeaderScrollView: View {
                 VStack(spacing: 0) {
                     GeometryReader { geometry -> AnyView in
                         let geometry = self.geometry(from: geometry, safeArea: globalGeometry.safeAreaInsets)
+                        headerStateCallback?(geometry.largeTitleWeight == 1)
                         return AnyView(
                             self.header
                                 .frame(width: geometry.width, height: geometry.headerHeight)
